d1898a2496d1142cfe7ab126c21366c7d4ab3068
ramvikash <vikashkarthik133@gmail.com>
2025-07-19 19:55:56 +0530

    index is changed to mjs file and server will run

--- index.mjs
+++ index.mjs
@@ -1,9 +1,13 @@
-const express = require('express');
-const puppeteer = require('puppeteer');
-const cors = require('cors');
+//const express = require('express');
+//const puppeteer = require('puppeteer');
+//const cors = require('cors');
+import express from 'express';
+import cors from 'cors';
+import scraperRoutes from './routes/scraperRoutes.js';
+import puppeteer from 'puppeteer';
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
