e24fca215ae58e258eb4d2b5292cb4b3a436bd7a
ramvikash <vikashkarthik133@gmail.com>
2025-07-22 22:18:56 +0530

    build-render.sh file is updated to install playwrite with permission, and package is updated to start with build

--- build-render.sh
+++ build-render.sh
@@ -1,3 +1,7 @@
-#!/usr/bin/env bash
-npx playwright install
-npm install
+#!/bin/bash
+
+# Give Playwright install script executable permission
+chmod +x ./node_modules/.bin/playwright
+
+# Install Playwright with system dependencies
+npx --yes playwright install --with-deps
