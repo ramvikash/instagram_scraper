3552e338c3834b31879f227204de2470f8e0387d
ramvikash <vikashkarthik133@gmail.com>
2025-07-22 20:40:23 +0530

    changed port to 5000

--- index.js
+++ index.js
@@ -43,5 +43,5 @@ app.post('/scrape', async (req, res) => {
   }
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
