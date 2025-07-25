988214e633b4711f3bdcde691d6087e6274c0457
ramvikash <68604155+ramvikash@users.noreply.github.com>
2025-07-25 21:11:34 +0530

    Update index.mjs

--- index.mjs
+++ index.mjs
@@ -44,4 +44,4 @@ app.post('/scrape', async (req, res) => {
 });
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(Server running on port ${PORT}));
+app.listen(PORT, () => console.log('Server running on port ${PORT}'));
